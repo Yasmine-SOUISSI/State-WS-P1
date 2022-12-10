144af842f7a89efd186a9e8d67e8836c06df914b
Yasmine-SOUISSI <yasmine.souissi@esprit.tn>
2022-12-10 12:42:17 +0100

    Add toggle btn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from "react";
 import "./App.css";
 import TodoList from "./Components/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    show: true,
+  };
+  render() {
+    return (
+      <div className="App">
+        <button onClick={() => this.setState({ show: !this.state.show })}>
+          Toggle
+        </button>
+        {this.state.show && <TodoList />}
+      </div>
+    );
+  }
 }
 
 export default App;
